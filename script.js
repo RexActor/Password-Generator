b8392d3eb8bd9a8f1ff0f11c38ffe1ffe81c7cd6
Gatis Jansons <bobsters.lol@gmail.com>
2024-01-05 19:56:38 +0000

    updated script for validation

--- script.js
+++ script.js
@@ -87,23 +87,36 @@ var upperCasedCharacters = [
   "Y",
   "Z",
 ];
-var lengthOfPassword = 0;
+
+//declaring some extra variables for checks and user input
+var lengthOfPassword;
 var hasSpecial = false;
 var hasNumeric = false;
 var hasLower = false;
 var hasUpperCase = false;
 
+//extra function for number check
+function isNumber(number) {
+  return !isNaN(parseInt(number));
+}
 // Function to prompt user for password options
 function getPasswordOptions() {
+  //asking for user to enter length of password and storing it in variable
   lengthOfPassword = prompt("How many characters you want for your password?");
 
-  if (parseInt(lengthOfPassword) < 8 || parseInt(lengthOfPassword) > 128) {
-    lengthOfPassword = alert(
-      "Length of Password should be at least 8 characters long and no more than 128 characters"
-    );
-    return;
+  //checking if input is number. Until user don't input valid number prompt will loop
+  while (!isNumber(lengthOfPassword)) {
+    lengthOfPassword = prompt("Please enter valid number from 8 to 128");
   }
 
+  //checking if length of the password is from 8 to 128 characters long. If not then prompt will be looped until user inputs correct value
+  while (parseInt(lengthOfPassword) < 8 || parseInt(lengthOfPassword) > 128) {
+    lengthOfPassword = prompt(
+      "Length of Password should be at least 8 characters long and no more than 128 characters"
+    );
+  }
+
+  //character selection confirmation windows
   hasLower = confirm("Do you want lower case characters in your password?");
   hasUpperCase = confirm("Do you want upper case characters in your password?");
   hasNumeric = confirm("Do you want Numbers in your password?");
@@ -119,24 +132,34 @@ function getRandom(arr) {
 // Function to generate password with user input
 function generatePassword() {
   getPasswordOptions();
+
+  //new array variable where all characters will be inserted if selected
   var charactersForPassword = [];
 
+  //checks if lower case characters were selected. If were then add this array into `charactersForPassword` array
   if (hasLower) {
     charactersForPassword = charactersForPassword.concat(lowerCasedCharacters);
   }
+  //checks if upper case characters were selected. If were then add this array into `charactersForPassword` array
   if (hasUpperCase) {
     charactersForPassword = charactersForPassword.concat(upperCasedCharacters);
   }
+  //checks if special characters were selected. If were then add this array into `charactersForPassword` array
   if (hasSpecial) {
     charactersForPassword = charactersForPassword.concat(specialCharacters);
   }
+  //checks if numbers were selected. If were then add this array into `charactersForPassword` array
   if (hasNumeric) {
     charactersForPassword = charactersForPassword.concat(numericCharacters);
   }
+
+  //If there are no characters selected in prompt selections, then `charactersForPassword` array will be empty and we are unable to generate password.
+  //  we are displaying in text fields that password can't be generated until selection is not made
   if (charactersForPassword.length == 0) {
-    return `Password can't be generated! Please select at least one character type when generating password`;
+    return `Password can't be generated! Please select at least one character type when generating password!`;
   }
 
+  //if above check is not being triggered, then we are generating password from `charactersForPassword` array with user inputted password length value as `lengthOfPassword`
 
   var generatedPassword = "";
   for (var i = 0; i < lengthOfPassword; i++) {
